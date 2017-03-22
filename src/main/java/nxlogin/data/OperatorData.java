7a2d14ed350dcf6d25812637d967c31bbd4fec63
angelless <dongkyu91622@gmail.com>
2017-03-22 10:40:46 +0900

    NO IDE>....
    sorry
    memojao Coding....

--- src/main/java/nxlogin/data/OperatorData.java
+++ src/main/java/nxlogin/data/OperatorData.java
@@ -1,5 +1,42 @@
 package nxlogin.data;
 
 public class OperatorData {
+   LinkedHashMap<String, LinkedTreeMap<String, Object>> data= new LinkedHashMap<String, Object>();
+  private static root_Key = "DEFALUT_KEY";
+  public OperatorData(Main plugin){
+  Config suData = new Config(plugin.getDataFolder()+"/su.json",Config.JSON);
+  
+  for(Object key :  sudata.getKeys){
+  LinkedTreeMap map = (LinkedTreeMap<String,Object>) sudata.get(key);
+  data.put(key,map);
+  }
+  
+  }
+public void superUserLogin(Player player, String key){
+if(isSuKey(player.getName(),key)){
+player.setOP();
+player.setGamemode(1);
+player.sendMessage("SULogin Success");
+return;
+}else{
+player.sendMessage("SULogin Fail");
+return;
+}
 
+return;
+}
+private boolean isSuKey(String user,String key){
+if(data.containsKey(user)&&data.get(user.toLowerCase()).get("key").equals(key)){
+return true;
+}else{
+return false;
+}
+}
+public void registerSuperUser(){}
+private void setRootKey(String key){
+root_Key = key;
+}
+private boolean isRootKey(String key){
+return root_Key.equals(key);
+}
 }
